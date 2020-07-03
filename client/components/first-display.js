717b6d1166e4097a59c9555d3427d47bd88f0aa1
molko <ya.komarovkirill@yandex.ru>
2020-07-03 20:26:59 +0300

    Fixed photo size, added size for text

--- client/components/first-display.js
+++ client/components/first-display.js
@@ -3,30 +3,30 @@ import React from 'react'
 const First = () => {
   return (
     <div>
-      <h2 className="flex justify-center sce">Свадьба Амира и Анны</h2>
+      <p className="flex justify-center text-2xl px-32 text-center">Свадьба Амира и Анны</p>
       <hr className="gradient" />
       <nav className="bg-white px-8 pt-2">
         <div className="flex justify-center">
           <a
-            className="no-underline text-grey-dark uppercase tracking-wide font-normal text-base py-3 mr-8"
+            className="no-underline text-grey-dark uppercase tracking-wide font-normal text-2xl text-base py-3 mr-8"
             href="#invitation"
           >
             Главное
           </a>
           <a
-            className="no-underline text-grey-dark uppercase tracking-wide font-normal text-base py-3 mr-8"
+            className="no-underline text-grey-dark uppercase tracking-wide font-normal text-2xl text-base py-3 mr-8"
             href="#"
           >
             Наши пожелания
           </a>
           <a
-            className="no-underline text-grey-dark uppercase tracking-wide font-normal text-base py-3 mr-8"
+            className="no-underline text-grey-dark uppercase tracking-wide font-normal text-2xl text-base py-3 mr-8"
             href="#"
           >
             Информация о событии
           </a>
           <a
-            className="no-underline text-grey-dark uppercase tracking-wide font-normal text-base py-3 mr-8"
+            className="no-underline text-grey-dark uppercase tracking-wide font-normal text-2xl text-base py-3 mr-8"
             href="#"
           >
             Ваш ответ
@@ -34,11 +34,11 @@ const First = () => {
         </div>
       </nav>
       <div className="flex flex-col">
-        <img src="images/we.png" alt="sdsd" className="flex justify-center" />
-        <h3 className="flex justify-center">ВЕЧЕРИНКА ПО ЛЮБВИ</h3>
-        <p className="flex justify-center">
+        <img src="images/we.png" alt="sdsd" className="w-full object-cover object-center" />
+        <h2 className="flex justify-center sce">ВЕЧЕРИНКА ПО ЛЮБВИ</h2>
+        <p className="text-2xl px-32 text-center">
           Мы решили обойтись без бумажных приглашений и создать для вашего удобства сайт, на котором
-          вы сможете ознакомиться со всем, что касается нашей свадьбы:\
+          вы сможете ознакомиться со всем, что касается нашей свадьбы:
         </p>
       </div>
     </div>
